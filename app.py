f40abc84ee39e5c7a116d4e38ca9ff4f340bc07f
zeb-27 <w.zeballosilva@gmail.com>
2025-12-19 21:24:57 -0300

    Refactor app.py for better readability and structure

--- app.py
+++ app.py
@@ -133,10 +133,9 @@ def eliminar_producto(id):
 
 # ARRANQUE DE LA APP
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
